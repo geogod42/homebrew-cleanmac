d66f998a3f90381747480ba239f8e7b6d37dca27
Jude Hoogterp <jhoogterp2016@gmail.com>
2026-01-11 22:24:19 -0800

    Update URLs to cleanmac.app domain

--- Casks/cleanmac.rb
+++ Casks/cleanmac.rb
@@ -2,10 +2,10 @@ cask "cleanmac" do
   version "0.1.0"
   sha256 "e4323563deb0244be12f08a793b3c417d187dcc783e0d5222aa2b89ed507614a"
 
-  url "https://d1b15qc2agq70j.cloudfront.net/cleanmac.dmg"
+  url "https://cleanmac.app/cleanmac.dmg"
   name "cleanmac"
-  desc "AI-powered disk cleanup for macOS"
-  homepage "https://d1b15qc2agq70j.cloudfront.net"
+  desc "Disk cleanup for macOS"
+  homepage "https://cleanmac.app"
 
   app "cleanmac.app"
 
